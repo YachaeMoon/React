9f2e71d7e4fad1c7cc79b362d2cbf7a8d1832dad
YachaeMoon <won2594@naver.com>
2024-01-30 16:05:13 +0900

    container

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import React from 'react';
 import ToDo from './ToDo';
-// import Sample from './Sample';
+import {Paper, List, Container} from "@material-ui/core"
+import AddToDo from './AddToDo';
 
 class App extends React.Component {
   constructor(props){
     super(props)
-    //this.state.time = {item:{id:0, "title":"Hello React", "done":true}}
     this.state = {items:[ {id:0, "title":"Hello React", "done":true}, 
                           {id:1, "title":"vue", "done":false}, 
                           {id:2, "title":"angular", "done":false}]
@@ -14,14 +14,22 @@ class App extends React.Component {
   }
 
   render(){
-    //배열을 순회하면서 출력할 내용을 생성
-    let display = this.state.items.map((item, idx) => ( //(아이템, 인덱스)
-        <ToDo item={item} key={item.id} /> //key={idx}해도 같음!
-    ));
+    var display = this.state.items.length > 0 && (
+      <Paper style={{margin:16}}>
+        <List>
+          {this.state.items.map((item, idx) => (
+            <ToDo item={item} key={idx} />
+          ))}
+        </List>
+      </Paper>
+    )
     
     return(
-        <div className="App">
+        <div className="AppToDo">
+          <Container maxWidth="md"> 
+            <AddToDo />
             {display}
+          </Container>
         </div>
     )
   }
